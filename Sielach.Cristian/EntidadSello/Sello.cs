7cf9ab510d06fe15c4e52a7b92b0e02b32835902
Cristian Sielach <crisielach@yahoo.com.ar>
2018-03-26 23:57:05 -0300

    Add files via upload

--- Sielach.Cristian/EntidadSello/Sello.cs
+++ Sielach.Cristian/EntidadSello/Sello.cs
@@ -14,15 +14,20 @@ namespace EntidadSello
 
         public static bool tryParse(string mensaje , out string devuelve)
         {
-            devuelve = "";
-            bool validacion=false;
-           
-            if(mensaje != "")
-           {
-               validacion = true;
-               devuelve=EntidadSello.Sello.ArmarFormatoMensaje();
-           }
-            return validacion;
+            bool resp = false;
+            int cant = mensaje.Length;
+
+            if(cant != 0)
+            {
+                devuelve = EntidadSello.Sello.ArmarFormatoMensaje();
+                resp = true;
+            }
+            else
+            {
+                devuelve = "";
+            }
+
+            return resp;
         }
         
         private static string ArmarFormatoMensaje()
@@ -36,19 +41,35 @@ namespace EntidadSello
             {
                 encuadre += "*";
             }
-            Console.Write(encuadre);
-            Console.Write("\n");
-            Console.Write("*");
-            Console.Write(EntidadSello.Sello.mensaje);
-            Console.Write("*\n");
-            Console.WriteLine(encuadre);
-            
+            encuadre += "\n";
+            encuadre += "*";
+            encuadre += Sello.mensaje;
+            encuadre += "*\n";
+
+            for (int i = 0; i < largo + 2; i++)
+            {
+                encuadre += "*";
+            }
+
+            return encuadre;
         }
 
         public static void imprimir()
         {
-            Console.ForegroundColor = Sello.color;
-            Sello.ArmarFormatoMensaje();
+            bool ok = tryParse(mensaje, out mensaje);
+
+            if(ok)
+            {
+                color = ConsoleColor.DarkRed;
+                Console.ForegroundColor = Sello.color;
+                Console.WriteLine("{0}", Sello.mensaje);
+                EntidadSello.Sello.color = ConsoleColor.DarkGray;
+                Console.ForegroundColor = EntidadSello.Sello.color;
+            }
+            else
+            {
+                Console.WriteLine("\nError\n");
+            }
         }
         
         public static void borrar()
